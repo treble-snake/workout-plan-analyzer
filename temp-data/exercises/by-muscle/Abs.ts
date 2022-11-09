0e1269f72e6bc27e7298e85c07bb2a7ecfcc5e19
treble-snake <treble.snake@gmail.com>
2022-11-09 19:20:16 +0200

    chore: add abs and arms exercises

--- temp-data/exercises/by-muscle/Abs.ts
+++ temp-data/exercises/by-muscle/Abs.ts
@@ -6,17 +6,47 @@ import {
   MovementType
 } from '../../../components/workout-viewer/analytics/systems-data/MovementTypeValues';
 
+const type = {
+  muscleGroup: MuscleGroup.Abs,
+  movementType: MovementType.Abs,
+};
+
 export const ABS_EXERCISES: ExerciseInfo[] = [
   {
     id: '85db96b9-a21e-4464-b43b-49682e77da66',
     name: 'Sit-ups',
-    muscleGroup: MuscleGroup.Abs,
-    movementType: MovementType.Abs,
+    ...type,
   },
   {
     id: 'bba8ce12-95f4-49ed-bee6-86f38365e986',
     name: 'Cable crunch',
-    muscleGroup: MuscleGroup.Abs,
-    movementType: MovementType.Abs,
+    ...type,
   },
+  {
+    id: '2325d1fb-ba31-41d3-aaf1-b7e0940230b5',
+    name: 'Hanging Knee Raise',
+    ...type,
+  },
+  {
+    id: 'b23ca2c1-8faf-4f80-acc2-8998d0dc2b99',
+    name: 'Hanging Straight Leg Raise',
+    ...type,
+  },
+  {
+    id: '52691f62-315f-41d9-a48a-5039e18b1fae',
+    name: 'Machine Crunch',
+    ...type,
+  },
+  {
+    id: '5917b6a7-876b-495e-82bc-7da39bf43de0',
+    name: 'V-up',
+    ...type,
+  },
+
+  {
+    id: '1173fbc8-907b-41c6-a75c-77225b5dddc5',
+    name: 'Generic abs exercise',
+    ...type,
+  },
+
 ];
--- temp-data/exercises/by-muscle/Arms.ts
+++ temp-data/exercises/by-muscle/Arms.ts
@@ -6,67 +6,117 @@ import {
   MovementType
 } from '../../../components/workout-viewer/analytics/systems-data/MovementTypeValues';
 
+const BICEPS_TYPE = {
+  muscleGroup: MuscleGroup.Biceps,
+  movementType: MovementType.Curl,
+};
+
+const TRICEPS_TYPE = {
+  muscleGroup: MuscleGroup.Triceps,
+  movementType: MovementType.Extend,
+};
+
 export const ARMS_EXERCISES: ExerciseInfo[] = [
   // biceps
   {
     id: '38952d79-84f7-486e-bf0a-cd01e30d9e8b',
     name: 'Dumbbell Hammer curl',
-    muscleGroup: MuscleGroup.Biceps,
-    movementType: MovementType.Curl,
+    ...BICEPS_TYPE
   },
   {
     id: '93828de1-4295-4e5d-9932-327e0faece29',
     name: 'Incline Dumbbell Curl',
-    muscleGroup: MuscleGroup.Biceps,
-    movementType: MovementType.Curl,
+    ...BICEPS_TYPE
   },
   {
     id: '9b9464c1-569f-4f7c-9ba1-ec3a24a0ff0b',
     name: 'Barbel curl',
-    muscleGroup: MuscleGroup.Biceps,
-    movementType: MovementType.Curl,
+    ...BICEPS_TYPE
   },
   {
     id: 'fd4d93d5-6db0-4922-bbdc-166fbed7e8e5',
     name: 'Reverse Grip Barbel curl',
-    muscleGroup: MuscleGroup.Biceps,
-    movementType: MovementType.Curl,
+    ...BICEPS_TYPE
   },
   {
     id: 'f0af0388-99e7-4442-95b4-1ac00ea9dbe0',
     name: 'Cable Hammer curl',
-    muscleGroup: MuscleGroup.Biceps,
-    movementType: MovementType.Curl,
+    ...BICEPS_TYPE
   },
   {
     id: 'f0af0388-99e7-4442-95b4-1ac00ea9dbe0',
     name: 'Cable curl',
-    muscleGroup: MuscleGroup.Biceps,
-    movementType: MovementType.Curl,
+    ...BICEPS_TYPE
+  },
+  {
+    id: 'acb35a2b-ec9e-4c89-a296-5246ec2ebdc1',
+    name: 'Spider curl',
+    ...BICEPS_TYPE
+  },
+  {
+    id: '2b624112-b197-4bc4-ba15-49894e02c226',
+    name: 'Preacher curl',
+    ...BICEPS_TYPE
+  },
+  {
+    id: '6864d89e-8da8-4eea-bd5a-810582657d98',
+    name: 'Pinwheel Curl',
+    ...BICEPS_TYPE
+  },
+  {
+    id: '2279459b-de8f-4b8c-b7c4-d8490cd552e4',
+    name: 'Generic biceps exercise',
+    ...BICEPS_TYPE,
   },
   // triceps
   {
     id: 'c8790bb9-0b73-4071-a454-c758ccec01cb',
     name: 'Triceps pushdown',
-    muscleGroup: MuscleGroup.Triceps,
-    movementType: MovementType.Extend,
+    ...TRICEPS_TYPE
   },
   {
     id: '59053cdf-0752-4517-9c87-5876391dddcf',
     name: 'Overhead Triceps Extension',
-    muscleGroup: MuscleGroup.Triceps,
-    movementType: MovementType.Extend,
+    ...TRICEPS_TYPE
   },
   {
     id: 'b461e945-47be-4b34-9bc4-9fd729fca05f',
     name: 'Diamond push-ups',
-    muscleGroup: MuscleGroup.Triceps,
-    movementType: MovementType.Extend,
+    ...TRICEPS_TYPE
   },
   {
     id: '3c7e50fa-0a8a-4be8-9d8e-5c02827afc18',
     name: '1-arm triceps pushdown',
-    muscleGroup: MuscleGroup.Triceps,
-    movementType: MovementType.Extend,
+    ...TRICEPS_TYPE
+  },
+  {
+    id: 'b7c12056-4052-4785-8586-92fdd3aa879b',
+    name: 'Dips',
+    ...TRICEPS_TYPE
+  },
+  {
+    id: 'b236975d-4b64-452c-81b3-c02c81752a18',
+    name: 'Skullcrusher',
+    ...TRICEPS_TYPE
+  },
+  {
+    id: 'e09236b8-e6d8-4431-9b16-403fdf01cee3',
+    name: 'Close Grip Bench Press',
+    ...TRICEPS_TYPE
+  },
+  {
+    id: 'ebdfb1ee-3b17-4040-9ec5-ab9c4a26279b',
+    name: 'JM Press',
+    ...TRICEPS_TYPE
+  },
+  {
+    id: '09ac8c2b-5275-4f80-9abd-260d9d27b976',
+    name: 'PRJ Pullover',
+    ...TRICEPS_TYPE
+  },
+  {
+    id: '417d48f3-57fd-4775-a8a4-86463ff0be3b',
+    name: 'Generic triceps exercise',
+    ...TRICEPS_TYPE,
   },
 ];
